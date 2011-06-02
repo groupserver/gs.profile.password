236048ac809620500737cde01994612d1d1050bd
Michael JasonSmith <mpj17@onlinegroups.net>
2011-06-02 14:00:53 +1000

    Verifying the email address when there is one address and it is unverified

--- gs/profile/password/reset.py
+++ gs/profile/password/reset.py
@@ -1,7 +1,10 @@
 # coding=utf-8
+from zope.cachedescriptors.property import Lazy
 from zope.component import createObject
 from zope.formlib import form
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
+from gs.profile.email.base.emailuser import EmailUser
+from gs.profile.email.verify.emailverificationuser import EmailVerificationUser
 from set import SetPasswordForm
 from interfaces import IGSPasswordUser
 
@@ -10,21 +13,44 @@ class ResetPasswordForm(SetPasswordForm):
     pageTemplateFileName = 'browser/templates/reset.pt'
     template = ZopeTwoPageTemplateFile(pageTemplateFileName)
 
+    @Lazy
+    def loggedInUser(self):
+        retval = createObject('groupserver.LoggedInUser', self.context)
+        assert not(retval.anonymous), 'Not logged in'
+        return retval
+        
     @form.action(label=u'Change', failure='handle_set_action_failure')
     def handle_set(self, action, data):
-       
-        # Using the logged in user, rather than self.context is
-        # deliberate. By using the logged in user we prevent anyone from
-        # even *accidently* changing the password of another user.
-        liu = createObject('groupserver.LoggedInUser', self.context)
-        assert not(liu.anonymous), 'Not logged in'
-        pu = IGSPasswordUser(liu)
-        pu.set_password(data['password1'])
-        pu.clear_password_reset()
-                
+        self.set_password(data['password1'])
+        self.verify_email()
+
         self.status = u'Your password has been changed. ' \
           'You can now '\
           '<strong>go to <a href="%s">your profile</a></strong> '\
           'to view your groups.' % self.userInfo.url
         assert type(self.status) == unicode
+
+    def set_password(self, password):
+        # Using the logged in user, rather than self.context is
+        # deliberate. By using the logged in user we prevent anyone from
+        # even *accidently* changing the password of another user.
+        pu = IGSPasswordUser(self.loggedInUser)
+        pu.set_password(password)
+        pu.clear_password_reset()
+
+    def verify_email(self):
+        # Ticket 480: Password Reset Should Verify Email Address
+        # <https://projects.iopen.net/groupserver/ticket/480>
+        # If A user only has one email address (most people),
+        #   and that address is unverified,
+        eu = EmailUser(self.context, self.loggedInUser)
+        if ((len(eu.get_addresses()) == 1) 
+            and (len(eu.get_unverified_addresses()) == 1)):
+            #  then the email address should be verified.
+            email = eu.get_addresses()[0]
+            evu = EmailVerificationUser(self.context, self.loggedInUser, 
+                                        email)
+            vid = '%s-password' % evu.create_verification_id()
+            evu.add_verification_id(vid)
+            evu.verify_email(vid)
 
