4a60d0b5ec2d893cd1a86c93ffc6d32c15aaa300
Michael JasonSmith <mpj17@onlinegroups.net>
2013-05-16 10:17:53 -0700

    Adding the password visibility toggle.

---
+++ gs/profile/password/browser/javascript/visible_toggle.js
@@ -0,0 +1,48 @@
+jQuery.noConflict();
+
+function GSProfilePasswordToggleVisibility (entryId, toggleId) {
+    var entry = null, 
+        toggle = null,
+        visible = false;
+
+    function set_visible () {
+        entry.attr('type', 'text');
+        visible = true;
+    }
+    
+    function set_hidden () {
+        entry.attr('type', 'password');
+        visible = false;
+    }
+
+    function get_visibility_value () {
+        return visibile;
+    }
+
+    function toggle_visibility() {
+        visible = ! visible;
+        if ( visible ) {
+            set_visible();
+        } else {
+            set_hidden();
+        }
+    }
+
+    function init () {
+        entry = jQuery(entryId);
+        toggle = jQuery(toggleId);
+        visible = Boolean(Number(toggle.val()));
+        if ( visible ) {
+            set_visible();
+        } else {
+            set_hidden();
+        }
+        toggle.change(toggle_visibility);
+        entry.focus();
+    }
+    init(); // Note the automatic execution
+
+    return {
+        get_visibility: function () { return get_visibility_value(); },
+    }
+} // GSProfilePasswordToggleVisibility
