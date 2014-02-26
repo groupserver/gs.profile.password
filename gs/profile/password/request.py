6c8240beb436a659d3795e9674aaee907de2939b
Michael JasonSmith <mpj17@onlinegroups.net>
2014-02-26 14:13:09 -0800

    Using the new notification

--- gs/profile/password/request.py
+++ gs/profile/password/request.py
@@ -13,19 +13,16 @@
 #
 ##############################################################################
 from __future__ import absolute_import, unicode_literals
-from md5 import new as new_md5
-from time import asctime
 from zope.cachedescriptors.property import Lazy
 from zope.formlib import form
 from Products.Five.browser.pagetemplatefile import ZopeTwoPageTemplateFile
 from gs.content.form import SiteForm
+from gs.core import to_id
 from gs.profile.email.base.emailaddress import address_exists
-from gs.profile.notify.interfaces import IGSNotifyUser
 from Products.CustomUserFolder.interfaces import IGSUserInfo
-from Products.XWFCore.XWFUtils import convert_int2b62, get_support_email
 from .audit import Auditor, REQUEST, REQUEST_FAIL
-from .createresetmessage import create_reset_message
 from .interfaces import IRequestPassword, IGSPasswordUser
+from .notifier import ResetNotifier
 
 
 class RequestPasswordResetForm(SiteForm):
@@ -36,15 +33,6 @@ class RequestPasswordResetForm(SiteForm):
 
     def __init__(self, context, request):
         super(RequestPasswordResetForm, self).__init__(context, request)
-
-    @Lazy
-    def abridgedSupport(self):
-        s = self.siteInfo.get_support_email()
-        if '@' not in s:
-            m = 'Support email address is not an email address.'
-            raise ValueError(m)
-        retval = '@{0}'.format(s.split('@')[1])
-        return retval
 
     @Lazy
     def auditor(self):
@@ -59,7 +47,6 @@ class RequestPasswordResetForm(SiteForm):
             # Send a reset message
             u = acl_users.get_userByEmail(email)
             userInfo = IGSUserInfo(u)
-            notifyUser = IGSNotifyUser(u)
             passwordUser = IGSPasswordUser(u)
 
             self.auditor.info(REQUEST, userInfo, email)
@@ -74,10 +61,9 @@ class RequestPasswordResetForm(SiteForm):
             #TODO: think about unverified addresses
 
             # send the message
-            fromAddr = get_support_email(self.context, self.siteInfo.id)
-            msg = create_reset_message(userInfo, self.siteInfo,
-                    email, fromAddr, resetId)
-            notifyUser.send_message(msg, email, fromAddr)
+            notifier = ResetNotifier(u, self.request)
+            resetLink = '{0}/r/password/{1}'.format(self.siteInfo.url, resetId)
+            notifier.notify(self.siteInfo, userInfo, resetLink)
 
             s = 'Instructions on how to reset your password have been sent '\
                 'to <code class="email">{0}</code>. Please check your email '\
@@ -93,13 +79,12 @@ class RequestPasswordResetForm(SiteForm):
 
     def handle_set_action_failure(self, action, data, errors):
         if len(errors) == 1:
-            self.status = 'There was an error:'
+            self.status = '<p>There was an error:</p>'
         else:
             self.status = '<p>There were errors:</p>'
 
     def get_reset_id(self, userInfo, email):
-        s = asctime() + email + userInfo.name + self.siteInfo.name
-        vNum = long(new_md5(s).hexdigest(), 16)
-        resetId = str(convert_int2b62(vNum))
-        assert resetId
-        return resetId
+        s = email + userInfo.name + self.siteInfo.name + \
+            self.siteInfo.get_support_email()
+        retval = to_id(s)
+        return retval
