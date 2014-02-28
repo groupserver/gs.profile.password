8e415539cd3067937ad3a946f27c2a84499b23f8
Michael JasonSmith <mpj17@onlinegroups.net>
2014-02-28 15:16:49 -0800

    Ensuring the oldContentType header is set.

--- gs/profile/password/notifier.py
+++ gs/profile/password/notifier.py
@@ -27,7 +27,7 @@ class ResetNotifier(object):
         self.context = self.user = user
         self.request = request
         h = self.request.response.getHeader('Content-Type')
-        self.oldContentType = to_ascii(h)
+        self.oldContentType = to_ascii(h if h else 'text/html')
 
     @Lazy
     def textTemplate(self):
